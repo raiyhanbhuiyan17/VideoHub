d9f7f9e829024d426fa7a4fd0dff6e5a12a0411c
raiyhan17 <r_bhui@live.concordia.ca>
2023-05-03 09:13:54 -0400

    initial commit

---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,79 @@
+import React,{useState, useEffect} from 'react';
+import {useParams,Link} from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Box,Stack,Typography, CircularProgress } from '@mui/material';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { CheckCircle } from '@mui/icons-material';
+
+
+const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const {id} =  useParams();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+    .then((data)=>{ //console.log(data)
+      setVideoDetail(data.items[0])}).finally(()=>setLoading(false));
+    // setDetail(data.items[0])
+
+    //console.log(data)
+  },[id])
+
+  if(!videoDetail?.snippet)return 'Loading...';
+
+  const channelTitle = videoDetail? videoDetail.snippet.channelTitle: "";
+  const title = videoDetail? videoDetail.snippet.title: "";
+  const channelId = videoDetail? videoDetail.snippet.channelId: "";
+
+  const viewCount = videoDetail? videoDetail.statistics.viewCount: "";
+  const likeCount = videoDetail? videoDetail.statistics.likeCount: "";
+  console.log("detail",videoDetail);
+  // const {statistics}= detail;
+  //const {snippet:{channelTitle}}= videoDetail;
+  //console.log("stat",statistics);
+  //console.log("ct",channelTitle);
+  
+
+  
+
+
+  return (
+    <Box minHeight='95vh'>
+      <Stack direction={{xs:'column', md:'row'}}>
+        <Box flex={1}>
+          <Box sx={{width:'100%', position:'sticky', top:'86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
+            className='react-player' controls/>
+            {loading && <CircularProgress />}
+            {!loading &&  <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>}
+           
+            <Stack direction='row' justifyContent='space-between' sx={{color:'#fff'}} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant={{sm:'subtitle',md:'h6'}} color='#fff'>
+                  {channelTitle}
+                  <CheckCircle sx={{fontSize:'12px', color:'gray', ml:'5px'}}/>
+                </Typography>
+              </Link>
+            <Stack direction='row' gap='19px' alignItems ='center'>
+              <Typography variant='body1' sx={{opacity:0.7}}>
+                {parseInt(viewCount).toLocaleString()} views
+              </Typography>
+              <Typography variant='body1' sx={{opacity:0.7}}>
+                {parseInt(likeCount).toLocaleString()} likes
+              </Typography>
+            </Stack>  
+            </Stack>
+          </Box>
+        </Box>
+
+      </Stack>
+
+    </Box>
+  )
+}
+
+export default VideoDetail
